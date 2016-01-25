e65d60b6951aad804ef5bc6ab4188dc6238174a5
Jamie Robert Dawson <jamierobertdawson@gmail.com>
2016-01-25 12:11:48 -0500

    First calculator

---
+++ Calculator/Form1.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.txtAnswer = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnSubtract = New System.Windows.Forms.Button()
+        Me.btnDivide = New System.Windows.Forms.Button()
+        Me.btnMultiply = New System.Windows.Forms.Button()
+        Me.btnModulus = New System.Windows.Forms.Button()
+        Me.btnPi = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.tlTipPi = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'txtNum1
+        '
+        Me.txtNum1.Location = New System.Drawing.Point(12, 39)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(357, 20)
+        Me.txtNum1.TabIndex = 0
+        '
+        'txtNum2
+        '
+        Me.txtNum2.Location = New System.Drawing.Point(12, 78)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(357, 20)
+        Me.txtNum2.TabIndex = 1
+        '
+        'txtAnswer
+        '
+        Me.txtAnswer.Location = New System.Drawing.Point(13, 124)
+        Me.txtAnswer.Name = "txtAnswer"
+        Me.txtAnswer.ReadOnly = True
+        Me.txtAnswer.Size = New System.Drawing.Size(357, 20)
+        Me.txtAnswer.TabIndex = 2
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(13, 159)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 3
+        Me.btnAdd.Text = "add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnSubtract
+        '
+        Me.btnSubtract.Location = New System.Drawing.Point(116, 159)
+        Me.btnSubtract.Name = "btnSubtract"
+        Me.btnSubtract.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubtract.TabIndex = 4
+        Me.btnSubtract.Text = "subtract"
+        Me.btnSubtract.UseVisualStyleBackColor = True
+        '
+        'btnDivide
+        '
+        Me.btnDivide.Location = New System.Drawing.Point(252, 159)
+        Me.btnDivide.Name = "btnDivide"
+        Me.btnDivide.Size = New System.Drawing.Size(75, 23)
+        Me.btnDivide.TabIndex = 5
+        Me.btnDivide.Text = "divide"
+        Me.btnDivide.UseVisualStyleBackColor = True
+        '
+        'btnMultiply
+        '
+        Me.btnMultiply.Location = New System.Drawing.Point(13, 206)
+        Me.btnMultiply.Name = "btnMultiply"
+        Me.btnMultiply.Size = New System.Drawing.Size(75, 23)
+        Me.btnMultiply.TabIndex = 6
+        Me.btnMultiply.Text = "multiply"
+        Me.btnMultiply.UseVisualStyleBackColor = True
+        '
+        'btnModulus
+        '
+        Me.btnModulus.Location = New System.Drawing.Point(116, 206)
+        Me.btnModulus.Name = "btnModulus"
+        Me.btnModulus.Size = New System.Drawing.Size(75, 23)
+        Me.btnModulus.TabIndex = 7
+        Me.btnModulus.Text = "modulus"
+        Me.btnModulus.UseVisualStyleBackColor = True
+        '
+        'btnPi
+        '
+        Me.btnPi.Location = New System.Drawing.Point(252, 206)
+        Me.btnPi.Name = "btnPi"
+        Me.btnPi.Size = New System.Drawing.Size(75, 23)
+        Me.btnPi.TabIndex = 8
+        Me.btnPi.Text = "pi"
+        Me.btnPi.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "First Number"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(10, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Second Number"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 99)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Answer"
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(16, 260)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 12
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(394, 306)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnPi)
+        Me.Controls.Add(Me.btnModulus)
+        Me.Controls.Add(Me.btnMultiply)
+        Me.Controls.Add(Me.btnDivide)
+        Me.Controls.Add(Me.btnSubtract)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtAnswer)
+        Me.Controls.Add(Me.txtNum2)
+        Me.Controls.Add(Me.txtNum1)
+        Me.Name = "Form1"
+        Me.Text = "Calculator "
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNum1 As TextBox
+    Friend WithEvents txtNum2 As TextBox
+    Friend WithEvents txtAnswer As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents btnDivide As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents btnModulus As Button
+    Friend WithEvents btnPi As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnClear As Button
+    Friend WithEvents tlTipPi As ToolTip
+End Class
---
+++ Calculator/Form1.vb
@@ -0,0 +1,173 @@
+﻿Public Class Form1
+    Private isAdding As Boolean = False
+    Private isSubtracting As Boolean = False
+    Private isDividing As Boolean = False
+    Private isMultiplying As Boolean = False
+    Private isModulus As Boolean = False
+
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        If txtNum1.Text.Length <> 0 And txtNum2.Text.Length <> 0 And isAdding = False Then
+            Dim answer As Double = add(CType(txtNum1.Text, Double), CType(txtNum2.Text, Double))
+            txtAnswer.Text = answer.ToString()
+            txtNum2.ReadOnly = True
+            isAdding = True
+        ElseIf isAdding Then
+            Dim ans As Double = add(CType(txtNum1.Text, Double), CType(txtAnswer.Text, Double))
+            txtAnswer.Text = ans.ToString()
+
+        Else
+            Dim MessageBoxIcons As Object = Nothing
+            MessageBox.Show("Please fill all fields", "error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+
+
+        End If
+
+
+    End Sub
+
+    Private Sub btnSubtract_Click(sender As Object, e As EventArgs) Handles btnSubtract.Click
+        If txtNum1.Text.Length <> 0 And txtNum2.Text.Length <> 0 And isSubtracting = False Then
+            Dim answer As Double = subtract(CType(txtNum1.Text, Double), CType(txtNum2.Text, Double))
+            txtAnswer.Text = answer.ToString()
+            txtNum2.ReadOnly = True
+            isSubtracting = True
+        ElseIf isSubtracting Then
+            Dim ans As Double = subtract(CType(txtNum1.Text, Double), CType(txtAnswer.Text, Double))
+            txtAnswer.Text = ans.ToString()
+
+        Else
+            Dim MessageBoxIcons As Object = Nothing
+            MessageBox.Show("Please fill all fields", "error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+
+
+        End If
+    End Sub
+
+    Private Sub btnDivide_Click(sender As Object, e As EventArgs) Handles btnDivide.Click
+        If txtNum1.Text.Length <> 0 And txtNum2.Text.Length <> 0 And isDividing = False Then
+            Dim answer As Double = divide(CType(txtNum1.Text, Double), CType(txtNum2.Text, Double))
+            txtAnswer.Text = answer.ToString()
+            txtNum2.ReadOnly = True
+            isDividing = True
+        ElseIf isDividing Then
+            Dim ans As Double = divide(CType(txtNum1.Text, Double), CType(txtAnswer.Text, Double))
+            txtAnswer.Text = ans.ToString()
+
+        Else
+            Dim MessageBoxIcons As Object = Nothing
+            MessageBox.Show("Please fill all fields", "error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+
+
+        End If
+
+
+    End Sub
+
+    Private Sub btnMultiply_Click(sender As Object, e As EventArgs) Handles btnMultiply.Click
+
+        If txtNum1.Text.Length <> 0 And txtNum2.Text.Length <> 0 And isMultiplying = False Then
+            Dim answer As Double = multiply(CType(txtNum1.Text, Double), CType(txtNum2.Text, Double))
+            txtAnswer.Text = answer.ToString()
+            txtNum2.ReadOnly = True
+            isMultiplying = True
+        ElseIf isMultiplying Then
+            Dim ans As Double = multiply(CType(txtNum1.Text, Double), CType(txtAnswer.Text, Double))
+            txtAnswer.Text = ans.ToString()
+
+        Else
+            Dim MessageBoxIcons As Object = Nothing
+            MessageBox.Show("Please fill all fields", "error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+
+
+        End If
+
+
+    End Sub
+
+    Private Sub btnModulus_Click(sender As Object, e As EventArgs) Handles btnModulus.Click
+        If txtNum1.Text.Length <> 0 And txtNum2.Text.Length <> 0 And isModulus = False Then
+            Dim answer As Double = modulus(CType(txtNum1.Text, Double), CType(txtNum2.Text, Double))
+            txtAnswer.Text = answer.ToString()
+            txtNum2.ReadOnly = True
+            isModulus = True
+        ElseIf isModulus Then
+            Dim ans As Double = modulus(CType(txtNum1.Text, Double), CType(txtAnswer.Text, Double))
+            txtAnswer.Text = ans.ToString()
+
+        Else
+            Dim MessageBoxIcons As Object = Nothing
+            MessageBox.Show("Please fill all fields", "error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+
+
+        End If
+
+
+    End Sub
+
+    Private Sub btnPi_Click(sender As Object, e As EventArgs) Handles btnPi.Click
+        If txtNum1.Text.Length <> 0 Then
+            Dim answer As Double = PI(CType(txtNum1.Text, Double))
+            txtAnswer.Text = String.Format("{0:n3}", answer)
+            txtNum2.Text = String.Format("{0: n3}", Math.PI)
+            txtNum2.ReadOnly = True
+            isAdding = True
+        ElseIf isAdding Then
+            Dim ans As Double = add(CType(txtNum1.Text, Double), CType(txtAnswer.Text, Double))
+            txtAnswer.Text = ans.ToString()
+
+        Else
+            Dim MessageBoxIcons As Object = Nothing
+            MessageBox.Show("Please fill all fields", "error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+
+
+        End If
+    End Sub
+
+    Private Function add(ByVal num1 As Double, ByVal num2 As Double) As Double
+        Return num1 + num2
+    End Function
+
+    Private Function subtract(ByVal num1 As Double, ByVal num2 As Double) As Double
+        Return num1 - num2
+    End Function
+
+    Private Function divide(ByVal num1 As Double, ByVal num2 As Double) As Double
+        Return num1 / num2
+    End Function
+
+    Private Function multiply(ByVal num1 As Double, ByVal num2 As Double) As Double
+        Return num1 * num2
+    End Function
+
+    Private Function modulus(ByVal num1 As Integer, ByVal num2 As Integer) As Integer
+        Return num1 Mod num2
+    End Function
+
+
+    Private Function PI(ByVal num1 As Double) As Double
+        Return num1 * Math.PI
+    End Function
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtNum2.ReadOnly = False
+        revertBool()
+        txtNum1.Clear()
+        txtNum2.Clear()
+        txtAnswer.Clear()
+
+
+    End Sub
+
+    Private Sub revertBool()
+        isAdding = False
+        isSubtracting = False
+        isDividing = False
+        isModulus = False
+        isMultiplying = False
+    End Sub
+
+    Private Sub btnPi_MouseHover(sender As Object, e As EventArgs) Handles btnPi.MouseHover
+        tlTipPi.SetToolTip(btnPi, "Only one value is needed in this first value field")
+    End Sub
+End Class
